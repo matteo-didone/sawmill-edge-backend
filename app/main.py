5407443ed8012d9dbb59eb12ca275501309b0071
Roberto <roberto.mio@stud.itsaltoadriatico.it>
2024-11-29 17:56:01 +0100

    updated main.py

--- app/main.py
+++ app/main.py
@@ -1,21 +1,24 @@
 import asyncio
 from asyncua import Client
 
-async def main():
-    # Endpoint del server OPC-UA
-    endpoint = "opc.tcp://192.168.100.53:4841/freeopcua/server/"
-    
-    # NodeId del nodo da leggere
-    node_id = {}
+# Funzione di connessione al server OPC-UA in base all'url inserito
+async def connectionToServer (connectionUrl):
 
-    node_id["machine_state"] = "ns=2;i=1"  # Sostituisci con il NodeId corretto per il nodo "pieces"
-    node_id["active"] = "ns=2;i=1"  # Sostituisci con il NodeId corretto per il nodo "active"
-    node_id["working"] = "ns=2;i=1"  # Sostituisci con il NodeId corretto per il nodo "working"
-    node_id["stopped"] = "ns=2;i=1"  # Sostituisci con il NodeId corretto per il nodo "stopped"
-    node_id["alarm"] = "ns=2;i=1"  # Sostituisci con il NodeId corretto per il nodo "alarm"
-    node_id["error"] = "ns=2;i=1"  # Sostituisci con il NodeId corretto per il nodo "error"
-    node_id["cutting_speed"] = "ns=2;i=1"  # Sostituisci con il NodeId corretto per il nodo "cutting_speed"
-    node_id["power_drain"] = "ns=2;i=1"  # Sostituisci con il NodeId corretto per il nodo "power_drain"
+    endpoint = connectionUrl
+
+    # NodeId del nodo da leggere
+    #node_id = {}
+
+    node_id = "ns=2;i=1"
+
+    #node_id["machine_state"] = "ns=2;i=1"  # Sostituisci con il NodeId corretto per il nodo "pieces"
+    #node_id["active"] = "ns=2;i=1"  # Sostituisci con il NodeId corretto per il nodo "active"
+    #node_id["working"] = "ns=2;i=1"  # Sostituisci con il NodeId corretto per il nodo "working"
+    #node_id["stopped"] = "ns=2;i=1"  # Sostituisci con il NodeId corretto per il nodo "stopped"
+    #node_id["alarm"] = "ns=2;i=1"  # Sostituisci con il NodeId corretto per il nodo "alarm"
+    #node_id["error"] = "ns=2;i=1"  # Sostituisci con il NodeId corretto per il nodo "error"
+    #node_id["cutting_speed"] = "ns=2;i=1"  # Sostituisci con il NodeId corretto per il nodo "cutting_speed"
+    #node_id["power_drain"] = "ns=2;i=1"  # Sostituisci con il NodeId corretto per il nodo "power_drain"
     
     previous_value = None  # Per tenere traccia dell'ultimo valore letto
     
@@ -48,8 +51,16 @@ async def main():
     finally:
         print("Programma terminato.")
 
+
+# Funzione Principale
+async def main():
+    # Endpoint del server OPC-UA
+    endpoint = "opc.tcp://192.168.100.53:4841/freeopcua/server/" 
+    asyncio.run(connectionToServer(endpoint))
+
+
 if __name__ == "__main__":
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        print("Chiusura in corso...")  # Gestisce CTRL+C
+        print("Chiusura in corso...")
